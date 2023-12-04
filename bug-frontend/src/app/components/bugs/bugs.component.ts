5486167f3e92dd863950083613c264279a1e8e09
JosueRay04 <147014434+JosueRay04@users.noreply.github.com>
2023-12-03 23:43:00 -0700

    9no Update

--- bug-frontend/src/app/components/bugs/bugs.component.ts
+++ bug-frontend/src/app/components/bugs/bugs.component.ts
@@ -1,10 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { BugService } from 'src/app/services/bug.service';
 
 @Component({
   selector: 'app-bugs',
   templateUrl: './bugs.component.html',
   styleUrls: ['./bugs.component.css']
 })
-export class BugsComponent {
 
+export class BugsComponent implements OnInit {
+  bugs: any[] = [];
+
+  constructor(private bugService: BugService) { }
+
+  ngOnInit(): void {
+    this.loadBugs();
+  }
+
+  loadBugs() {
+    this.bugService.getBugs().subscribe(
+      (data) => {
+        this.bugs = data;
+
+        // Aquí puedes acceder a la información del usuario para cada bug
+        this.bugs.forEach(bug => {
+          console.log('User ID:', bug.user.id);
+          console.log('User Name:', bug.user.name);
+          // ... otras propiedades del usuario
+        });
+      },
+      (error) => {
+        console.error('Error fetching bugs:', error);
+      }
+    );
+  }
 }
